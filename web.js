e5d890463e52d70825f2ba157fdc42473d0bd454
Dan Kane <dkane@electricswarm.com>
2013-07-16 17:24:18 +0000

    put buff inside bracket

--- web.js
+++ web.js
@@ -1,14 +1,12 @@
-var fs = require('fs');
 var express = require("express");
 var app = express();
 
 app.get('/', function(request, response) {
 
-var buff = new Buffer(256); 
+var buff = new Buffer(256);
 buff = fs.readFileSync("index.html");
-var string = buff.toString();  
 
-response.send(string);
+response.send(buff.toString());
 
  } );
 
